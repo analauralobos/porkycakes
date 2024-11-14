bd9cf425952b5af0dc27904d2efac38791dbbde4
roleg <rochileg@hotmail.com>
2024-11-14 09:48:52 -0300

    primer commit

--- Frontend/adminfront/src/components/login/Login.js
+++ Frontend/adminfront/src/components/login/Login.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css'; // Para estilos personalizados
+import './Login.css';
 
-function Login({ setUserRole }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+function Login({ closeModal, setUserRole }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
   const handleLogin = async () => {
@@ -32,41 +32,41 @@ function Login({ setUserRole }) {
     } catch (error) {
       console.error('Error de login:', error);
       alert('Hubo un error al intentar hacer login');
-        }
-    };
+    }
+  };
 
-    return (
-    <div className="login-container d-flex justify-content-center align-items-center">
-      <div className="login-card">
-        <div className="card-body text-center">
-          <h2 className="mb-4 text-gradient">Iniciar sesión</h2>
+  return (
+    <div className="login-modal-overlay" onClick={closeModal}> {/* Cierra el modal si se hace clic fuera */}
+      <div className="login-modal" onClick={(e) => e.stopPropagation()}> {/* Previene el cierre si se hace clic dentro del modal */}
+        <div className="login-card">
+          <h2 className="text-gradient">Iniciar sesión</h2>
           <div className="form">
-          <div className="form-group mb-3">
-                    <input
-                        type="email"
-              className="form-control"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-              placeholder="Correo electrónico"
-                    />
-                </div>
-          <div className="form-group mb-3">
-                    <input
-                        type="password"
-              className="form-control"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-              placeholder="Contraseña"
-                    />
-                </div>
+            <div className="form-group mb-3">
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Correo electrónico"
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Contraseña"
+              />
+            </div>
           </div>
-          <button className="btn-pink " onClick={handleLogin}>
+          <button className="btn-pink" onClick={handleLogin}>
             Iniciar sesión
           </button>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Login;
