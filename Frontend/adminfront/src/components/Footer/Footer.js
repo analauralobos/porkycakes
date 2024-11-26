ee5b52d12deee9865f3d1c820e6d3dc2e9b8c03e
Ana Laura Lobos <anhalaurha@gmail.com>
2024-11-26 18:38:38 -0300

    footer al fondo---

--- Frontend/adminfront/src/components/Footer/Footer.js
+++ Frontend/adminfront/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import './Footer.css'
 import { FaInstagram, FaFacebook, FaEnvelope, FaPhone } from 'react-icons/fa';
 
@@ -46,3 +46,39 @@ const Footer = () => {
 }
 
 export default Footer
+*/
+
+import React from 'react';
+import './Footer.css';
+import { FaInstagram, FaFacebook, FaEnvelope, FaPhone } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <div className="container-footer">
+            <div className="footer-section">
+                <p className="encabezado">Redes Sociales:</p>
+                <div className="footer-item">
+                    <FaInstagram className="icono" />
+                    <p className="contenido">porky.cakes</p>
+                </div>
+                <div className="footer-item">
+                    <FaFacebook className="icono" />
+                    <p className="contenido">Porky Cakes</p>
+                </div>
+            </div>
+            <div className="footer-section">
+                <p className="encabezado">Contacto:</p>
+                <div className="footer-item">
+                    <FaEnvelope className="icono" />
+                    <p className="contenido">porky_cakes@outlook.com</p>
+                </div>
+                <div className="footer-item">
+                    <FaPhone className="icono" />
+                    <p className="contenido">2302-657698</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
